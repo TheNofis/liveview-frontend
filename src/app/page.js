c374a649ed4447ebf5d3ab735a1eb787e5d770e5
TheNofis <matbey122@gmail.com>
2024-10-11 15:28:11 +0300

    add container

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,59 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { io } from "socket.io-client";
+
+import Welcome from "./components/Welcome";
+import Header from "./components/Header";
+import CodeBlockList from "./components/CodeBlockList";
 
 export default function Home() {
   const [socket, setSocket] = useState(io("localhost:3001"));
+  const [codeData, setCodeData] = useState(null);
+  const [username, setUsername] = useState("hi");
+  const [userData, setUserData] = useState(null);
+
+  const iframe = useRef(null);
+
   useEffect(() => {
     socket.on("connect", () => {
       socket.emit("clientConnect", socket.id);
-      setInterval(() => {
-        console.log(123);
-        socket.emit("startLiveServer", socket.id);
-      }, 1000);
-
       console.log("connect");
     });
+
     socket.on("fileContent", (data) => {
-      console.log(data);
+      if (codeData == null) return setCodeData(data);
+
+      Object.keys(data).forEach((key) => {
+        const dataNow = JSON.parse(JSON.stringify(codeData));
+        dataNow[key] = data[key];
+        setCodeData(dataNow);
+
+        // Update iframe code
+        if (!iframe?.current?.contentWindow?.location) return;
+        iframe.current.src += "";
+      });
+    });
+    socket.on("connectInfo", (data) => {
+      setUserData(data);
     });
   });
-  return <div>123</div>;
+
+  return (
+    <div className="container">
+      <Header username={username} />
+
+      <div className="codeblock-list">
+        {codeData != null ? <CodeBlockList codeData={codeData} /> : <></>}
+      </div>
+      <div class="iframe">
+        <iframe
+          src="http://localhost:3002"
+          ref={iframe}
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </div>
+  );
 }
