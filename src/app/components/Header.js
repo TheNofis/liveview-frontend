ea24d46826deec9f93b4bf2238484b3c3f354a86
TheNofis <matbey122@gmail.com>
2024-10-11 19:45:22 +0300

    add header__connlist

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -9,17 +9,30 @@ export default function Header({ socket }) {
   return (
     <header className="header">
       <div className="header__name">LiveServer</div>
-      <div className="header__profile">
-        {userData?.username ? (
-          <>
-            <div className="header__profile__avatar">
-              {userData?.username[0]}
-            </div>
-            <span className="header__profile__name">{userData?.username}</span>
-          </>
-        ) : (
-          <></>
-        )}
+      <div className="header__info">
+        <div className="header__connlist">
+          {userList != null
+            ? userList.map((user) => (
+                <div className="header__connlist__user" key={user.id}>
+                  {user?.username[0]}
+                </div>
+              ))
+            : null}
+        </div>
+        <div className="header__profile">
+          {userData?.username ? (
+            <>
+              <div className="header__profile__avatar">
+                {userData?.username[0]}
+              </div>
+              <span className="header__profile__name">
+                {userData?.username}
+              </span>
+            </>
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
     </header>
   );
